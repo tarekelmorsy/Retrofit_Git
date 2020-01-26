56cb11a02bd0ef56bc5c749999f25e0a8b619b37
tarek elmorsy <tarekelmorsy208@gmail.com>
2020-01-26 23:41:02 +0200

    initial commit

---
+++ app/src/main/java/com/example/data/PostInterface.java
@@ -0,0 +1,16 @@
+package com.example.data;
+
+
+import com.example.model.PostModel;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface PostInterface {
+    @GET("posts")
+    public Call<List<PostModel>> getPosts();
+
+}
+
